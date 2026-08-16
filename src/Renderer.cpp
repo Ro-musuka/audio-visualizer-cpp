50a45d3f754e49ad18fcc0d09c5299210e97d397
Ro-musuka <nfyj89dsq2@privaterelay.appled.com>
2026-08-16 10:42:37 +0900

    背景色変更

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -85,7 +85,7 @@ void Renderer::Update(float deltaTime)
 
 void Renderer::Draw()
 {
-    glClearColor(0.3f, 0.2f, 0.3f, 1.0f);
+    glClearColor(0.6f, 0.1f, 0.1f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     mGUI->Draw();
